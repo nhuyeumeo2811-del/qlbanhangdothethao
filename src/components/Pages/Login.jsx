1a1397dcebd006f0dc58926913e3ca0adf29ddac
nhuyeumeo2811-del <nhuyeumeo2811@gmail.com>
2026-06-15 01:43:02 +0700

    Update Login.jsx

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -63,7 +63,7 @@ const Login = () => {
             if (matchedAccount.role === 'staff') {
                 navigate('/admin');
             } else {
-                navigate('/admin');
+                navigate('/');
             }
         } catch (err) {
             console.error(err);
